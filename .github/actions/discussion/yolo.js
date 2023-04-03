4cce82a22207e144565d9ecd1d5b72adf6732d14
pseudoc <atlas.yu@canonical.com>
2023-04-03 12:02:19 +0800

    arch: requests and questions

--- .github/actions/discussion/yolo.js
+++ .github/actions/discussion/yolo.js
@@ -2,11 +2,9 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function main() {
-  const event = github.context.eventName;
   const action = github.context.action;
   const payload = github.context.payload;
 
-  core.info(`Event: ${event}`);
   core.info(`Action: ${action}`);
   core.info(`Payload: ${JSON.stringify(payload, null, 2)}`);
 }
