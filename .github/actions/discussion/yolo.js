7d250546122b1fd1661bc5d679d0ad4f746d7ad0
pseudoc <atlas.yu@canonical.com>
2023-04-03 14:59:48 +0800

    skip when alread commented

--- .github/actions/discussion/yolo.js
+++ .github/actions/discussion/yolo.js
@@ -1,13 +1,13 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 const categories = {
-  request: 'DIC_kwDOJJzAY84CVZ_1',
-  question: 'DIC_kwDOJJzAY84CVZ_2',
+  request: "DIC_kwDOJJzAY84CVZ_1",
+  question: "DIC_kwDOJJzAY84CVZ_2",
 };
 
 async function request() {
-  const octokit = github.getOctokit(core.getInput('token'));
+  const octokit = github.getOctokit(core.getInput("token"));
   const payload = github.context.payload;
   const number = payload.discussion.number;
   const author = payload.discussion.user.login;
@@ -17,13 +17,16 @@ async function request() {
       discussionId: payload.discussion.node_id,
       body: `@${author}`,
     };
-    return octokit.graphql(`mutation($input: AddDiscussionCommentInput!) {
+    return octokit.graphql(
+      `mutation($input: AddDiscussionCommentInput!) {
       addDiscussionComment(input: $input) {
         comment {
           id
         }
       }
-    }`, { input });
+    }`,
+      { input },
+    );
   }
 
   function updateComment(commentId, numbers) {
@@ -32,40 +35,50 @@ async function request() {
       body: [
         `@${author}`,
         ...numbers.map((number) => `- [ ] #${number}`),
-      ].join('\n'),
+      ].join("\n"),
     };
-    return octokit.graphql(`mutation($input: UpdateDiscussionCommentInput!) {
+    return octokit.graphql(
+      `mutation($input: UpdateDiscussionCommentInput!) {
       updateDiscussionComment(input: $input) {
         comment {
           id
         }
       }
-    }`, { input });
+    }`,
+      { input },
+    );
   }
 
-  function createQuestion(cbNodeId, current, total) {
+  const NUMBER_OF_QUESTIONS = 2;
+  function ask(cbNodeId, current, total = NUMBER_OF_QUESTIONS) {
     const input = {
       repositoryId: payload.repository.node_id,
       title: `Request #${number} from ${author} (${current}/${total})`,
       body: [
         `Request: #${number} @${author}`,
         `Callback: ${cbNodeId}`,
-      ].join('\n'),
+      ].join("\n"),
       categoryId: categories.question,
     };
-    return octokit.graphql(`mutation($input: CreateDiscussionInput!) {
+    return octokit.graphql(
+      `mutation($input: CreateDiscussionInput!) {
       createDiscussion(input: $input) {
         discussion {
           number
         }
       }
-    }`, { input });
+    }`,
+      { input },
+    );
   }
 
-  const { comment } = await addComment();
+  const { addDiscussionComment: { comment } } = await addComment();
+  console.log(JSON.stringify(data, null, 2));
+
   const discussions = [];
-  for (let i = 1; i <= 2; i++) {
-    const { discussion } = await createQuestion(comment.id, i, 2);
+
+  for (let i = 1; i <= NUMBER_OF_QUESTIONS; i++) {
+    const { createDiscussion: { discussion } } = await ask(comment.id, i);
     discussions.push(discussion.number);
   }
   await updateComment(comment.id, discussions);
@@ -74,7 +87,10 @@ async function request() {
 async function main() {
   const action = github.context.action;
   switch (action) {
-    case 'request':
+    case "request":
+      if (github.context.payload.discussion.comments !== 0) {
+        core.setSkipped("Already commented");
+      }
       await request();
       break;
     default:
@@ -83,7 +99,7 @@ async function main() {
 }
 
 try {
-  main();
+  await main();
 } catch (error) {
   console.log(JSON.stringify(github.context.payload, null, 2));
   core.setFailed(error.message);
